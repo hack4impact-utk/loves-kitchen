850b5c50898ea63d7e67feeec24e4e62f218dc7b
maddyyu <127363085+maddyyu@users.noreply.github.com>
2024-10-18 09:55:30 -0400

    logo centered, 2 links added

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,19 +4,41 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
 import Image from 'next/image';
+import Button from '@mui/material/Button';
+// import { Link } from '@mui/material';
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="inherit">
-        <Toolbar>
-          <Image
-            src="/lk-logo.jpg"
-            alt="Love Kitchen Logo"
-            width={100}
-            height={40}
-          />
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Box sx={{ display: 'flex', gap: 2 }}>
+            <Button color="inherit">Link 1</Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+            {/* <Link href="/" passHref> */}
+            <Image
+              src="/lk-logo.jpg"
+              alt="Love Kitchen Logo"
+              width={100}
+              height={40}
+              layout="intrinsic"
+            />
+            {/* </Link> */}
+          </Box>
+
+          <Box sx={{ display: 'flex', gap: 2 }}>
+            <Button color="inherit">Link 2</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
