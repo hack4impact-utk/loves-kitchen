752c0816e01acbc252eaeab8da9269acdea2ad46
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2024-11-17 22:37:53 -0500

    created user page with hardcoded values

--- src/components/UserComp/index.tsx
+++ src/components/UserComp/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState } from 'react';
 import { Box, Button, Typography } from '@mui/material';
 
---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import React, { useState } from 'react';
+import {
+  Box,
+  Typography,
+  Alert,
+  Button,
+  createTheme,
+  ThemeProvider,
+} from '@mui/material';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
+import ErrorIcon from '@mui/icons-material/Error';
+import CheckIcon from '@mui/icons-material/Check';
+import SearchIcon from '@mui/icons-material/Search';
+
+const UserInfo = () => {
+  const { user, error, isLoading } = useUser();
+  const [isCheckedIn, setIsCheckedIn] = useState(false);
+
+  const toggleCheckIn = () => {
+    setIsCheckedIn((prev) => !prev);
+  };
+
+  if (error) {
+    console.log('Error: failed to load user credentials.');
+  }
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1e88d4', // Primary color for MUI components
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {typeof user != 'undefined' ? (
+        <>
+          <Box className="bg-slate-700 flex flex-col items-center px-3 py-5 rounded-xl w-[50vw] relative">
+            {' '}
+            {/* LOGIN SUCCESSFULL */}
+            <Image
+              src={user.picture ?? ''}
+              alt={user.name ?? ''}
+              width={100}
+              height={100}
+              priority
+              className="rounded-full m-5"
+            />
+            <Typography variant="h5" className="mt-2 text-2xl text-white">
+              {user.name}
+            </Typography>
+            <Typography variant="body1" className="text-[#a7a7a7]">
+              {user.email}
+            </Typography>
+            {/* Alert using MUI that displays if a user is checked in. */}
+            <Alert
+              icon={
+                isCheckedIn ? (
+                  <CheckIcon fontSize="inherit" />
+                ) : (
+                  <ErrorIcon fontSize="inherit" />
+                )
+              }
+              className="my-3"
+              severity={isCheckedIn ? 'success' : 'warning'} // Change severity based on status
+            >
+              {isCheckedIn ? 'Checked in.' : 'Not checked in.'}{' '}
+              {/* Change display message */}
+            </Alert>
+            <Button
+              variant="contained"
+              onClick={toggleCheckIn}
+              className="absolute bottom-5 left-5"
+            >
+              {isCheckedIn ? 'Check Out' : 'Check In'}
+            </Button>
+          </Box>
+        </>
+      ) : isLoading ? (
+        <>
+          {' '}
+          {/* LOGIN PENDING */}
+          <Box className="bg-slate-700 flex flex-col items-center px-3 py-5 rounded-xl w-[50vw] relative min-h-[316px]" />
+        </>
+      ) : (
+        <>
+          {' '}
+          {/* ERROR OR LOGGED OUT */}
+          <Box className="bg-slate-700 flex flex-col items-center justify-center px-3 py-5 rounded-xl w-[50vw] relative min-h-[316px]">
+            <SearchIcon fontSize="large" color="primary" />
+            <Typography className="text-2xl text-white">
+              Hmm, can&apos;t tell who you are.
+            </Typography>
+            <Typography className="text-lg text-neutral-300 mb-5">
+              Try logging in!
+            </Typography>
+          </Box>
+        </>
+      )}
+    </ThemeProvider>
+  );
+};
+
+export default UserInfo;
