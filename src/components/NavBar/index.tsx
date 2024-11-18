f126e27af43264909b8845791c49f7abe1f9b591
maddyyu <127363085+maddyyu@users.noreply.github.com>
2024-11-17 21:23:44 -0500

    fancy box as seen on LK page

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,14 +4,34 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-// import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 import Button from '@mui/material/Button';
-// import { Link } from '@mui/material';
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, position: 'relative' }}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          backgroundColor: '#fff',
+          padding: 2,
+          borderRadius: 4,
+          zIndex: 1,
+        }}
+        onClick={() => (window.location.href = '/')}
+      >
+        <Image
+          src="/lk-logo.jpg"
+          alt="Love Kitchen Logo"
+          width={200}
+          height={40}
+          layout="intrinsic"
+        />
+      </Box>
+
       <AppBar position="static" color="inherit">
         <Toolbar
           sx={{
@@ -27,18 +47,6 @@ export default function ButtonAppBar() {
             <Button color="inherit" href="/staff">
               staff
             </Button>
-          </Box>
-
-          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-            {/* <Link href="/" passHref> */}
-            <Image
-              src="/lk-logo.jpg"
-              alt="Love Kitchen Logo"
-              width={100}
-              height={40}
-              layout="intrinsic"
-            />
-            {/* </Link> */}
           </Box>
 
           <Box sx={{ display: 'flex', gap: 2 }}>
