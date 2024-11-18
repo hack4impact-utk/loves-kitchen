83ada4972761c2bc09c9a2ddd7506f5cdf1d28f2
andybenson24 <andy.benson0424@gmail.com>
2024-11-17 23:17:11 -0500

    wrote script to generate 2 collections, correctly formatted, didnt have time to make it generate 2-5 sessions for each individual

--- src/server/fill/fillDb.py
+++ src/server/fill/fillDb.py
@@ -1,39 +1,50 @@
 import pymongo
 from datetime import datetime
-import time
 import random
-import pandas as pd
+import names
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["dummy_base"]
+usercol = mydb["users"]
+sesh = mydb["sessions"]
 
-# Random time selector function
-# adapted from here: https://stackoverflow.com/questions/553303/how-to-generate-a-random-date-between-two-other-dates
-def random_time(start, end, time_format, prop):
-    """Get a time at a proportion of a range of two formatted times.
+firstNamesArr = []
+lastNamesArr = []
+ages = []
+emails = []
+elapsed = []
+times = []
+for i in range(100):
+    Fname = names.get_first_name()
+    firstNamesArr.append(Fname)
 
-    start and end should be strings specifying times formatted in the
-    given format (strftime-style), giving an interval [start, end].
-    prop specifies how a proportion of the interval to be taken after
-    start.  The returned time will be in the specified format.
-    """
+    Lname = names.get_last_name()
+    lastNamesArr.append(Lname)
 
-    stime = time.mktime(time.strptime(start, time_format))
-    etime = time.mktime(time.strptime(end, time_format))
+    age = random.randint(16,75)
+    ages.append(age)
 
-    ptime = stime + prop * (etime - stime)
+    email = "%s.%s@gmail.com" % (firstNamesArr[i].lower(), lastNamesArr[i].lower())
+    emails.append(email)
 
-    return time.localtime(ptime)
+    startTime = datetime.now()
+    times.append(startTime)
 
+    hours = random.uniform(0.5, 8.0)
+    hours = round(hours * 2) / 2     #rounds to half hours
+    elapsed.append(hours)
 
-if __name__ == "__main__":
-    # establish connection
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["dummy_base"]
-    mycol = mydb["vols"]
+#after populating arrays, bind everything together for 2 collections
 
-    # read csv
-    fileData = pd.read_csv("dummy.csv")
+people = [
+    {"name": f"{Fname} {Lname}", "age": age, "email": email, "CreatedAt":startTime}
+    for Fname, Lname, age, email, startTime in zip(firstNamesArr, lastNamesArr, ages, emails, times) #binds each array at index i to each other
+]
+seshbind = [
+    {"name": f"{Fname} {Lname}", "Start Time":startTime, "Shift length":hours}
+    for Fname, Lname, startTime, hours in zip(firstNamesArr, lastNamesArr, times, elapsed) #binds each array at index i to each other
+]
 
-    for i in range(len(fileData.index)):
-        struct = random_time("4/16/2004 1:30 PM", "10/13/2024 4:50 AM", '%m/%d/%Y %I:%M %p', random.random())
-        dt = datetime.fromtimestamp(time.mktime(struct))
-        mycol.insert_one({"name": fileData.iloc[i]["name"], "age": fileData.iloc[i]["age"].item(), "createdAt": dt})
+if((usercol.find_one() == None) or (people[0]["name"] != usercol.find_one()["name"])): #runs if empty or if first index doesnt equal each other
+    usercol.insert_many(people)
+    sesh.insert_many(seshbind)
