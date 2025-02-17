9f7055a92d0d9e8d7d61583faeb00d2f9f4b0cd0
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2025-02-17 15:53:12 -0500

    must sign in to visit QR generator page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ['/user', '/user/:path*', '/staff'],
+  matcher: ['/user', '/user/:path*', '/staff', '/staff/:path*'],
 };
